11a5f96a58852ead62c0aa118d579b9f807603b0
Innis <1575270674@qq.com>
2025-02-14 15:08:04 +0800

    feat: add routers

---
+++ src/pages/remove-group/remove-group.tsx
@@ -0,0 +1,7 @@
+export default function RemoveGroup() {
+    return (
+      <div>
+        <h1>Remove Group</h1>
+      </div>
+    );
+}
--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -3,6 +3,9 @@ import App from "../App";
 import CGDashboard from "../modules/Cg";
 import Callback from "../modules/_callbacks";
 import { ProtectedRoute } from "../components/ProtectedRoute";
+import AssignGroup from "../pages/assign-group/assign-group.tsx";
+import BindAccount from "../pages/bind-account/bind-account.tsx";
+import RemoveGroup from "../pages/remove-group/remove-group.tsx";
 
 export const routes = [
   {
@@ -20,6 +23,21 @@ export const routes = [
     element: <Callback />,
     protected: false,
   },
+    {
+        path: "/users/:userId/assign-group",
+        element: <AssignGroup/>,
+        protected: true,
+    },
+    {
+        path: "/users/:userId/bind-account",
+        element: <BindAccount/>,
+        protected: true,
+    },
+    {
+        path: "/users/:userId/remove-group",
+        element: <RemoveGroup/>,
+        protected: true,
+    }
 ];
 
 export const MainRoutes = () => {
